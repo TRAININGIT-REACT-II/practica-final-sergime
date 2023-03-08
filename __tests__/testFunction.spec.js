c571e9695aac2c5aae4b5ebe20a191a449caf912
Sergi Mas <sergi.mas@gmail.com>
2023-03-08 20:33:00 +0100

    Add more tests

--- __tests__/testFunction.spec.js
+++ __tests__/testFunction.spec.js
@@ -14,4 +14,19 @@ describe('#Helper functions', () => {
 
     expect(shortenedText).toHaveLength(30)
   })
+
+  it('returns the same string if the string is smaller than the passed length', () => {
+    const text = "Small text"
+    const shortenedText = shortenText(text, 30)
+
+    expect(shortenedText).toBe(text)
+  })
+
+  it('returns the same string if passed length is zero or negative', () => {
+    const text = "This is a very long text where I will try the shorten function"
+    const shortenedText = shortenText(text, -1)
+    expect(shortenedText).toBe(text)
+    const shortenedText2 = shortenText(text, 0)
+    expect(shortenedText2).toBe(text)
+  })
 });
--- client/src/notes/helpers/shortenText.js
+++ client/src/notes/helpers/shortenText.js
@@ -1,4 +1,10 @@
 
 export const shortenText = (text, nchars = 20) => {
+  if ((nchars - 1) < 0) {
+    return text
+  }
+  if (text.length < nchars) {
+    return text
+  }
   return text.slice(0, nchars - 1) + '…'
 }
